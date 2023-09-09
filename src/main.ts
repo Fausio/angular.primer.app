92fbbcd1fa34b1f72e358ce6d29d7599daed0409
fausio luis <luisfausio@gmail.com>
2023-09-09 17:40:58 +0200

    Optional Function Parameters

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-  function writeValue(val?: string | null) {
+  function writeValue(val?: string ) {
     console.log(`Value: ${val ?? "Fallback value"}`)
     }
 
     writeValue("London");
-    writeValue(null);
+    writeValue();
