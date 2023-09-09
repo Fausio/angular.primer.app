71d8a382ee102e5bafeaa70fd898779b9f349c41
fausio luis <luisfausio@gmail.com>
2023-09-09 18:04:24 +0200

    Spread Operator

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,10 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
 
 
-let myArray: (number | string | boolean)[] = [100, "Adam", true];
-for (let i = 0; i < myArray.length; i++) {
-  console.log("Index " + i + ": " + myArray[i]);
-}
-console.log("---");
-myArray.forEach((value, index) => console.log("Index " + index + ": " + value));
+  let myArray: (number | string | boolean)[] = [100, "Adam", true];
+  let otherArray = [...myArray, 200, "Bob", false];
+  // for (let i = 0; i < myArray.length; i++) {
+  // console.log("Index " + i + ": " + myArray[i]);
+  // }
+  // console.log("---");
+  otherArray.forEach((value, index) => console.log("Index " + index + ": " + value));
