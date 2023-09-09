ac495be64fb7cb4135e2565ccb56c7a41c46893d
fausio luis <luisfausio@gmail.com>
2023-09-09 18:25:27 +0200

    Methods

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,28 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 class Product {
+
+  name: string;
+  price: number;
+  category?: string;
+
   constructor(name: string, price: number, category?: string) {
     this.name = name;
     this.price = price;
     this.category = category;
   }
-  name: string
-  price: number
-  category?: string
+
+
+
+
+  printDetails() {
+    if (this.category != undefined) {
+      console.log(`Name: ${this.name}, Price: ${this.price}, ` +
+        `Category: ${this.category}`);
+    } else {
+      console.log(`Name: ${this.name}, Price: ${this.price}`);
+    }
+  }
 }
 
 let hat = new Product("Hat", 100);
@@ -22,17 +36,9 @@ let boots = new Product("Boots", 100, "Snow Gear");
 
 
 
-function printDetails(product: { name: string, price: number, category?: string }) {
-  if (product.category != undefined) {
-    console.log(`Name: ${product.name}, Price: ${product.price}, ` +
-      `Category: ${product.category}`);
-  } else {
-    console.log(`Name: ${product.name}, Price: ${product.price}`);
-  }
-}
 
-printDetails(hat);
-printDetails(boots);
+
+hat.printDetails();
 
 
 
