4d887ee51e6e2d186fcf749e9fade0072d942a5b
fausio luis <luisfausio@gmail.com>
2023-09-09 10:30:36 +0200

    Dealing with Unassigned and Null Values

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,9 @@ const age: number = 40;
 
 let place;
 console.log("Place value: " + place + " Type: " + typeof(place));
+
 place = "London";
+console.log("Place value: " + place + " Type: " + typeof(place));
+
+place = null;
 console.log("Place value: " + place + " Type: " + typeof(place));
