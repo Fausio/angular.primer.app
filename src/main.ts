1c39e8680328f0da79742ed4e4eeb21dd875e2fa
fausio luis <luisfausio@gmail.com>
2023-09-09 10:28:38 +0200

    Dealing with Unassigned and Null Values

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
-function myFunction(param: number | string) {
-  /*  if (typeof (param) == "number" || typeof (param) == "string") {
-    // let result = (param as any) + 100;
-    let fixed = param.toFixed(2);
-    console.log("My result: " + fixed);
-   } else {
-     throw ("Expected a number: " + param)
-   } */
+let condition: boolean = true;
+let person: string = "Bob";
+const age: number = 40;
 
-  if (typeof (param) == "number") {
-    let numberResult = param.toFixed(2);
-    console.log("My result: " + numberResult);
-  } else {
-    let stringResult = param + 100;
-    console.log("My result: " + stringResult);
-  }
-}
-myFunction(1);
-myFunction("London");
+
+
+let place;
+console.log("Place value: " + place + " Type: " + typeof(place));
+place = "London";
+console.log("Place value: " + place + " Type: " + typeof(place));
