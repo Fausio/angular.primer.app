4f208569a1fe982271463c5ec9c32f5d66d6db17
fausio luis <luisfausio@gmail.com>
2023-09-09 18:55:54 +0200

    Observable, Observer and Subject

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-import { Name, WeatherLocation } from "./modules/NameAndWeather";
-let name = new Name("Adam", "Freeman");
-let loc = new WeatherLocation("raining", "London");
-console.log(name.nameMessage);
-console.log(loc.weatherMessage);
+import { Observable, Observer, Subject } from "rxjs";
+
+function recieveEvents(observable: Observable<string>) {
+  observable.subscribe({
+    next: str => {
+      console.log(`Event received: ${str}`);
+    },
+    complete: () => console.log("Sequence ended")
+  });
+}
+
+function sendEvents(observer: Observer<string>) {
+  let count = 5;
+  for (let i = 0; i < count; i++) {
+    observer.next(`${i + 1} of ${count}`);
+  }
+  observer.complete();
+}
+
+let subject = new Subject<string>();
+
+recieveEvents(subject);
+sendEvents(subject);
